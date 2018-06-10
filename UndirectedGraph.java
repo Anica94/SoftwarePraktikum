1e1bcff89da3b28036e63530eeb65917dd500180
SPMT94 <39301328+SPMT94@users.noreply.github.com>
2018-06-10 20:35:45 +0200

    Update UndirectedGraph.java

--- UndirectedGraph.java
+++ UndirectedGraph.java
@@ -1,17 +1,20 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.HashMap;
 import java.util.TreeMap;
 import java.util.NoSuchElementException;
 
+
 /*
- * The graph being implemented is undirected meaning that <v,>) in E does mean that <u,v> in E.
+ * The graph being implemented is undirected meaning that <v,> in E does mean that <u,v> in E.
  */
 public class UndirectedGraph implements Graph {
 	
 	/*
 	 * Hashmap containing all vertices as keys and their respective incident edges as elements 
 	 * organized in a treemap (or red-black-tree) where the endpoints of an edge are the keys 
-	 * and the weight of this edge is the respective value.
+	 * and the weight of this edge is the respective value. Note all edges are saved twice.
 	 */
 	private HashMap<Integer, TreeMap<Integer, Integer>> startpoints;
 	
@@ -29,10 +32,10 @@ public class UndirectedGraph implements Graph {
 	}
 
 	@Override
-	public void addVertex() {
+	public Integer addVertex() {
 		Integer vertexNameDefault = new Integer(highestVertexName+1);
 		this.addVertex(vertexNameDefault);
-
+		return vertexNameDefault;
 	}
 
 	@Override
@@ -40,8 +43,11 @@ public class UndirectedGraph implements Graph {
 		if (vertexName == null) {
     		throw new NullPointerException();
     	}
-		// TODO Auto-generated method stub
-		return false;
+		if (startpoints.containsKey(vertexName)) {
+			return false;
+		}
+		startpoints.put(vertexName, new TreeMap<Integer, Integer>());
+		return true;
 	}
 
 	@Override
@@ -49,7 +55,9 @@ public class UndirectedGraph implements Graph {
 		if (vertexName == null) {
     		throw new NullPointerException();
     	}
-		// TODO Auto-generated method stub
+		if (startpoints.containsKey(vertexName)) {
+			return true;
+		}
 		return false;
 	}
 
@@ -58,8 +66,25 @@ public class UndirectedGraph implements Graph {
 		if (vertexName == null) {
     		throw new NullPointerException();
     	}
-		// TODO Auto-generated method stub
-		return false;
+		if (startpoints.containsKey(vertexName)) {
+			return false;
+		}
+		/* iterate through all adjacent vertices of vertexName, 
+		 * 		in each case remove vertexName from the respective adjacent vertices, 
+		 * remove vertexName
+		 */
+		TreeMap<Integer, Integer> adjacent = startpoints.get(vertexName);
+		Collection c = adjacent.values();
+	    Iterator itr = c.iterator();
+
+	    while (itr.hasNext()){
+	    	Integer  currentVertex = (Integer) itr.next();
+	    	TreeMap<Integer, Integer> currentAdjacent = startpoints.get(currentVertex);
+	    	currentAdjacent.remove(vertexName);
+	    	startpoints.put(currentVertex, currentAdjacent);
+	    }
+	    startpoints.remove(vertexName);
+		return true;
 	}
 
 	@Override
@@ -70,10 +95,15 @@ public class UndirectedGraph implements Graph {
 		if (!startpoints.containsKey(vertexNameStart) || !startpoints.containsKey(vertexNameEnd) || vertexNameStart.equals(vertexNameEnd)) {
     		throw new IllegalArgumentException();
     	}
-		// TODO Auto-generated method stub
+		// check adjacent vertices of vertexNameStart
+		TreeMap<Integer, Integer> adjacent = startpoints.get(vertexNameStart);
+		if (adjacent.containsKey(vertexNameEnd))
+		{
+			return true;
+		}
 		return false;
 	}
-
+// TODO ///////////
 	@Override
 	public boolean addEdge(Integer vertexNameStart, Integer vertexNameEnd) {
 		if (vertexNameStart == null || vertexNameEnd == null) {
@@ -159,3 +189,4 @@ public class UndirectedGraph implements Graph {
 	}
 
 }
+
