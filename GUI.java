de7796306070f484b2064dbcd023400e2134e8e4
Anica94 <39301272+Anica94@users.noreply.github.com>
2018-06-10 21:59:42 +0200

    Add files via upload
    I added the current version of GUI, will still work on it, but wanted to save it

--- GUI.java
+++ GUI.java
@@ -1,4 +1,5 @@
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
 import java.awt.Image;
 
 import javax.swing.JFrame;
@@ -8,6 +9,7 @@ import java.awt.BorderLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -31,6 +33,7 @@ import java.awt.Color;
 public class GUI {
 
 	private JFrame frame;
+	private static DrawPanel drawPanel;
 
 	/**
 	 * Launch the application.
@@ -60,10 +63,16 @@ public class GUI {
 	 */
 	private void initialize() {
 		frame = new JFrame("GraphGUI");
-		frame.getContentPane().setBackground(new Color(224, 255, 255));
+		//frame.getContentPane().setBackground(new Color(224, 255, 255));
 		frame.setBounds(100, 100, 800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		drawPanel = new DrawPanel();
+		drawPanel.setBorder(null);
+		drawPanel.setLocation(0,0);
+		drawPanel.setSize(784,514);
+		frame.getContentPane().add(drawPanel);
 		
 		JLabel lblStatus = new JLabel("Status");
 		lblStatus.setBounds(0, 515, 784, 25);
@@ -78,21 +87,57 @@ public class GUI {
 		
 		JMenuItem mntmBreadthFirstSearch = new JMenuItem("Breadth-First Search");
 		mnAlgorithm.add(mntmBreadthFirstSearch);
+		mntmBreadthFirstSearch.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				lblStatus.setText("Runs Breadth-First Search");
+			}
+		});
 		
 		JMenuItem mntmDepthfirstsearch = new JMenuItem("Depth-First Search");
 		mnAlgorithm.add(mntmDepthfirstsearch);
+		mntmDepthfirstsearch.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				lblStatus.setText("Runs Depth-First Search");
+			}
+		});
 		
 		JMenuItem mntmTopologicalSort = new JMenuItem("Topological Sort");
 		mnAlgorithm.add(mntmTopologicalSort);
+		mntmTopologicalSort.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				lblStatus.setText("Runs Topological Sort");
+			}
+		});
 		
-		JMenuItem mntmMaximumSpanningTree = new JMenuItem("Minimum Spanning Tree");
-		mnAlgorithm.add(mntmMaximumSpanningTree);
+		JMenuItem mntmMinimumSpanningTree = new JMenuItem("Minimum Spanning Tree");
+		mnAlgorithm.add(mntmMinimumSpanningTree);
+		mntmMinimumSpanningTree.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				lblStatus.setText("Runs Minimum Spanning Tree");
+			}
+		});
 		
 		JMenuItem mntmMaximalMatching = new JMenuItem("Maximal Matching");
 		mnAlgorithm.add(mntmMaximalMatching);
+		mntmMaximalMatching.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				lblStatus.setText("Runs Maximal Matching");
+			}
+		});
 		
 		JMenuItem mntmBuild = new JMenuItem("BUILD");
 		mnAlgorithm.add(mntmBuild);
+		mntmBuild.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				lblStatus.setText("Runs BUILD");
+			}
+		});
 		
 		JMenu mnEdit = new JMenu("Edit");
 		menuBar.add(mnEdit);
@@ -100,20 +145,45 @@ public class GUI {
 		JMenu mnAdd = new JMenu("add");
 		mnEdit.add(mnAdd);
 		
-		JMenuItem mntmNewMenuItem = new JMenuItem("vertex (draw)");
-		mnAdd.add(mntmNewMenuItem);
+		JMenuItem mntmVertexdraw = new JMenuItem("vertex (draw)");
+		mnAdd.add(mntmVertexdraw);
+		mntmVertexdraw.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				lblStatus.setText("Add Vertex");
+			}
+		});
 		
 		JMenuItem mntmVertexspecify = new JMenuItem("vertex (specify)");
 		mnAdd.add(mntmVertexspecify);
 		
 		JMenuItem mntmEdgedraw = new JMenuItem("edge (draw)");
 		mnAdd.add(mntmEdgedraw);
+		mntmEdgedraw.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				lblStatus.setText("Add Edge");
+			}
+		});
 		
 		JMenuItem mntmEdgespecify = new JMenuItem("edge (specify)");
 		mnAdd.add(mntmEdgespecify);
+		mntmEdgespecify.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				createAddLineFrame();
+				lblStatus.setText("add Edge");
+			}
+		});
 		
 		JMenuItem mntmDelete = new JMenuItem("delete");
 		mnEdit.add(mntmDelete);
+		mntmDelete.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				lblStatus.setText("Delete");
+			}
+		});
 		
 		JMenuItem mntmMove = new JMenuItem("move");
 		mnEdit.add(mntmMove);
@@ -154,6 +224,42 @@ public class GUI {
 		
 		JMenu mnHelp = new JMenu("Help");
 		menuBar.add(mnHelp);
+	}
+	
+	public static void createAddLineFrame() {
+		JFrame frame = new JFrame();
+		frame.setLayout(new FlowLayout());
+		frame.setBounds(100, 150, 200, 75);
+		
+		JTextField txtC1 = new JTextField(1);
+		JTextField txtC2 = new JTextField(1);
+		
+		JButton btnCancel = new JButton("cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+			}
+		});
+		
+		JButton btnAddLine = new JButton("add");
+		btnAddLine.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int cid1 = Integer.parseInt(txtC1.getText());
+				int cid2 = Integer.parseInt(txtC2.getText());
+				drawPanel.addLine(cid1, cid2);
+				frame.dispose();
+			}
+		});
+		
+		frame.add(txtC1);
+		frame.add(txtC2);
+		frame.add(btnCancel);
+		frame.add(btnAddLine);
+		frame.setVisible(true);
 	}
 }
 
