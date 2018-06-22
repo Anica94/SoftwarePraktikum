a0dd035334418b28adfc59e6b459a64e4f3c555a
Anica94 <39301272+Anica94@users.noreply.github.com>
2018-06-22 21:19:23 +0200

    Update DrawPanel.java

--- DrawPanel.java
+++ DrawPanel.java
@@ -15,43 +15,52 @@ import javax.swing.JPanel;
 import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class DrawPanel extends JPanel {
-	private UndirectedGraph graph;
+	private Graph graph;
+	private String typeOfGraph;
 	private ArrayList<Integer> vertices;
 	private Integer vertex;
 	private int x, y, x2, y2;
-	private HashMap<Integer, Integer> vertexX;
-	private HashMap<Integer, Integer> vertexY;
 	private HashMap<Integer, Pair<Integer, Integer> > vertexCoordinates;
 	private Pair<Integer, Integer> coordinates;
 	private Pair<Integer, Integer> coordinates2;
-	//private HashMap<Integer, Integer> coordinates;
 	private Integer newVertex;
 	private Integer startpoint;
 	private Circle c;
-	private ArrayList<Circle> circles;
-	private ArrayList<Line> lines;	
+	private int status;
+	//private Graphics g;
 	
 	private int mouseX, mouseY;
 	private JLabel lblMouseCoords;
 	
-	public DrawPanel() {
-		graph = new UndirectedGraph();
+	public DrawPanel(Graph graph, String typeOfGraph) {
+		this.graph = graph;
+		this.typeOfGraph = typeOfGraph;
 		vertexCoordinates = new HashMap<Integer, Pair<Integer, Integer> >();
 		coordinates = new Pair<Integer, Integer>(0, 0);
 		coordinates2 = new Pair<Integer, Integer>(0, 0);
-		vertexX = new HashMap<Integer, Integer>();
-		vertexY = new HashMap<Integer, Integer>();
 		startpoint = null;
-		circles = new ArrayList<Circle>();
-		lines = new ArrayList<Line>();
-//		this.setBorder(BorderFactory.createLineBorder(Color.black, 2, false));
+/*
+		System.out.println("number of vertices " + graph.getVertices().size());
+		if(!graph.getVertices().isEmpty() && vertexCoordinates.isEmpty()) {
+			createDefaultCoordinates();
+		}*/
+		//repaint();
+		
 		
 		this.addMouseMotionListener(new MouseMotionListener() {
 
 			@Override
 			public void mouseDragged(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
+				/*
+				 * move
+				 */
+				if(GUI.getStatusNumber()==12) {
+					//vertex = findVertex(x, y);
+					changeCoordinates(vertex, e.getX(), e.getY());
+					//coordinates = new Pair<Integer, Integer>(mouseX, mouseY);
+					//vertexCoordinates.replace(vertex, coordinates);
+				}
+				repaint();
 			}
 
 			@Override
@@ -72,21 +81,25 @@ public class DrawPanel extends JPanel {
 
             @Override
             public void mousePressed(MouseEvent e) {
+            	status = GUI.getStatusNumber();
+            	switch (status) {
             	/*
             	 * add vertex
             	 */
-            	if(GUI.getStatusNumber()==7) {
-            		newVertex = graph.addVertex();
+				case 7:
+					System.out.println("case addVertex");
+					newVertex = graph.addVertex();
+					System.out.println("newVertex = "+ newVertex);
+					System.out.println("number of vertices " + graph.getVertices().size());
             		coordinates = new Pair<Integer, Integer>(mouseX, mouseY);
-            		//coordinates.setFirst(mouseX);
-            		//coordinates.setSecond(mouseY);
             		vertexCoordinates.put(newVertex, coordinates);
-            		
-            		//vertexX.put(newVertex, mouseX);
-            		//vertexY.put(newVertex, mouseY);
-            	}
-            	else if(GUI.getStatusNumber()==8){
-            		vertex = findVertex(mouseX, mouseY);
+            		System.out.println("vertexCoordniates.size() = " + vertexCoordinates.size());
+					break;
+				/*
+            	 * add edge by drawing
+            	 */
+				case 8:
+					vertex = findVertex(mouseX, mouseY);
             		if(startpoint == null) {
             			startpoint = vertex;
             		}
@@ -94,11 +107,38 @@ public class DrawPanel extends JPanel {
             			addEdge(startpoint, vertex);
             			startpoint = null;
             		}
-            	}
-            	else if(GUI.getStatusNumber()==10) {
-            		vertex = findVertex(mouseX, mouseY);
+            		break;
+        		/*
+        		 * delete vertex
+        		 */
+				case 10:
+					vertex = findVertex(mouseX, mouseY);
             		graph.deleteVertex(vertex);
-            	}
+            		break;
+        		/*
+        		 * delete edge
+        		 */
+				case 11: 
+					vertex = findVertex(mouseX, mouseY);
+            		if(startpoint == null) {
+            			startpoint = vertex;
+            		}
+            		else {
+            			graph.deleteEdge(startpoint, vertex);
+            			startpoint = null;
+            		}
+            		break;
+        		/*
+        		 * move
+        		 */
+				case 12:
+					x = mouseX;
+            		y = mouseY;
+            		vertex = findVertex(x, y);
+            		break;
+				default:
+					break;
+				}            	
             	repaint();
             }
 
@@ -108,16 +148,24 @@ public class DrawPanel extends JPanel {
         });
 	}
 	
-	/*
-	 * computes the distance of two points (with x and y coordinates)
+	/**
+	 * Computes the distance of two points (with x and y coordinates).
+	 * 
+	 * @param x1 x-coordinate of the first point.
+	 * @param y1 y-coordinate of the first point.
+	 * @param x2 x-coordinate of the second point.
+	 * @param y2 y-coordinate of the second point.
 	 */
 	public double distance(int x1, int y1, int x2, int y2){
 		double distance = Math.sqrt(Math.pow((x1-x2),2)+Math.pow((y1-y2), 2));
 		return distance;
 	}
 	
-	/*
-	 * finding a vertex at given coordinates
+	/**
+	 * Finds a vertex at given coordinates.
+	 * 
+	 * @param x1 x-coordinate 
+	 * @param y2 y-coordinate 
 	 */
 	public Integer findVertex(int x1, int y1) {
 		Integer v = null;
@@ -141,26 +189,87 @@ public class DrawPanel extends JPanel {
 		}
 		return v;
 	}
-/*	
-	public void addLine(int cid1, int cid2) {
-		if (cid1 < 0 || cid2 < 0 || cid1 >= circles.size() || cid2 >= circles.size()) {
-			throw new IllegalArgumentException("Ids must be valid");
+	
+	/**
+	 * Adds an edge to the graph.
+	 * 
+	 * @param vertexNameStart name of the startpoint of the edge to be added.
+	 * @param vertexNameEnd name of the endpoint of the edge to be added.
+	 */
+	public void addEdge(Integer vertexNameStart, Integer vertexNameEnd) {
+		graph.addEdge(vertexNameStart, vertexNameEnd);
+		repaint();
+	}
+
+	/**
+	 * Changes the actual graph (if a graph is read from a file or deleted).
+	 * 
+	 * @param graph the new graph.
+	 * @param typeOfGraph the type of the new graph.
+	 */
+	public void changeGraph(Graph graph, String typeOfGraph) {
+		this.graph = graph;
+		this.typeOfGraph = typeOfGraph;
+		System.out.println("vertexCoordniates.size() = " + vertexCoordinates.size());
+		vertexCoordinates.clear();
+		System.out.println("number of vertices " + this.graph.getVertices().size());
+		System.out.println("vertexCoordniates.size() = " + vertexCoordinates.size());
+		if(!graph.getVertices().isEmpty() && vertexCoordinates.isEmpty()) {
+			createDefaultCoordinates();
 		}
-		lines.add(new Line(circles.get(cid1), circles.get(cid2)));
+		System.out.println("vertexCoordniates.size() = " + vertexCoordinates.size());
 		repaint();
 	}
+	
+	/**
+	 * Creates default coordinates for a graph which is read from a file.
+	 */
+	public void createDefaultCoordinates() {
+		vertices = graph.getVertices();
+		for(int i=0; i<vertices.size(); i++) {
+			vertex = vertices.get(i);
+			int x = (int)(Math.random() * ((784) + 1));
+			int y = (int)(Math.random() * ((514) + 1));
+			coordinates = new Pair<Integer, Integer>(x, y);
+			vertexCoordinates.put(vertex, coordinates);
+		}
+	}
+	
+	/**
+	 * Changes the coordinates of a given vertex.
+	 * 
+	 * @param vertex name of the vertex of which the coordinates should be changed.
+	 * @param x new x-coordinate of the vertex.
+	 * @param y new y-coordinate of the vertex.
+	 */
+	public void changeCoordinates(Integer vertex, int x, int y) {
+		coordinates = new Pair<Integer, Integer>(x, y); 
+		vertexCoordinates.replace(vertex, coordinates);
+	}
+/*	
+	private void drawPoint(Integer vertexName, int x, int y) {
+        // Neues Grafik-Objekt aus dem altem Erzeugen und darauf dann die
+        // Grafik-Funktionen nutzen.
+		
+		int r = 10;
+        Graphics g = this.getGraphics();
+        //g.fillOval(x, y, 3, 3);
+        g.setColor(Color.BLUE);
+		g.fillOval(x-r, y-r, r*2, r*2);
+		g.setColor(Color.WHITE);
+		g.drawString(String.valueOf(vertexName), x-3, y+3);
+		repaint();
+    }
 */	
-	public void addEdge(Integer cid1, Integer cid2) {
-		graph.addEdge(cid1, cid2);
-		repaint();
-	}
-	
-	
 	@Override
     protected void paintComponent (Graphics g) {
 		super.paintComponent(g);
 		
+		Integer vertex;
+		Pair<Integer, Integer> coordinates;
+		
 		vertices = graph.getVertices();
+		System.out.println("number of vertices = " + vertices.size());
 		for(int i=0; i<vertices.size(); i++) {
 			vertex = vertices.get(i);
 			coordinates = vertexCoordinates.get(vertex);
@@ -178,9 +287,10 @@ public class DrawPanel extends JPanel {
 		    }
 			c = new Circle(vertex, 10, x, y);
 			c.draw(g);
+		    //drawPoint(vertex, x, y);
 			
 		}
 		
-		
 	}
+	
 }
