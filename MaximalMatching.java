e8786882cc90db2d4116738c2cac5b2604566b37
SPMT94 <39301328+SPMT94@users.noreply.github.com>
2018-06-24 00:09:48 +0200

    Update MaximalMatching.java

--- MaximalMatching.java
+++ MaximalMatching.java
@@ -3,46 +3,85 @@ import java.util.HashMap;
 import java.util.TreeMap;
 
 public class MaximalMatching implements Algorithm {
-	
+	/**
+	 * Saves the result of the algorithm as a graph.
+	 */
 	private Graph result;
 
+	/**
+	 * Produces an algorithm for maximal matching with an empty result.
+	 */
 	public MaximalMatching() {
 		result = null;
 	}
 
+
 	@Override
 	public ArrayList<Option> execute(Graph graph) {
-		// MaxMAtching Alg aus Folie
-		ArrayList<Option> changes = new ArrayList();
-		Graph copyGraph = graph; // E'
+		if (graph == null) {
+    		throw new NullPointerException();
+    	}
+		/*
+		 * make copy of the graph to work on (contains E')
+		 * produce container of variables to use later
+		 * produce arraylist of options that will be done by the algorithm
+		 */
+		Graph copyGraph = graph;
 		ArrayList<Integer> copyVertices = copyGraph.getVertices();
+		HashMap<Integer, TreeMap<Integer, Integer>> copyStartpoints = copyGraph.getStartpoints();
 		Integer currentVertex;
-		// produce M as graph without any edges
+		Integer startVertexName = null;
+		Integer endVertexName = null;
+		ArrayList<Option> changes = new ArrayList<Option>();
+		/*
+		 * produce the empty matching (M) as graph without any edges
+		 */
+		if(copyGraph.typeOfGraph().equals("undirected")) {
+			result = new UndirectedGraph();
+		}
+		else {
+			result = new DirectedGraph();
+		}
 		for(int i = 0; i < copyVertices.size(); i++) {
 			currentVertex = copyVertices.get(i);
 			result.addVertex(currentVertex);
 		}
-		
+		/*
+		 * do maximal matching
+		 * return arraylist of options
+		 */
 		while (copyGraph.containsEdges()) {
-			Integer startVertexName = null; // wird wegen while Bed aber zu etwas
-			Integer endVertexName = null;
+			/*
+			 * find random edge <startVertexName, endVertexName> in copyGraph (<u,v> in E'):
+			 * for all vertices
+			 * 		if there is an adjacent vertex
+			 * 			choose the corresponding edge
+			 * delete this edge in the copyGraph (update E')
+			 * memorize this step as an option 
+			 */
 			for(int i = 0; i < copyVertices.size(); i++) {
 				currentVertex = copyVertices.get(i);
-				HashMap<Integer, TreeMap<Integer, Integer>> copyStartpoints = copyGraph.getStartpoints();
-				if (copyStartpoints.containsKey(currentVertex)) // also gibt es einen adjazenten Knoten zum current
+				if (copyStartpoints.containsKey(currentVertex))
 				{
 					startVertexName = currentVertex;
-					endVertexName = copyStartpoints.get(currentVertex).firstKey(); // beliebige Kante
+					endVertexName = copyStartpoints.get(currentVertex).firstKey();
 					break;
 				}
 			}
+			copyGraph.deleteEdge(startVertexName, endVertexName);
 			changes.add(new EdgeOption("consider", startVertexName, endVertexName));
-			//füge diese Kante zu M  hinzu  und lösche sie aus copyGraph
+			/*
+			 * add this edge to the matching / result to be (update M)
+			 * memorize this step as an option
+			 */  			
 			result.addEdge(startVertexName, endVertexName);
-			copyGraph.deleteEdge(startVertexName, endVertexName);
-			copyGraph.deleteEdge(startVertexName, endVertexName);
 			changes.add(new EdgeOption("choose", startVertexName, endVertexName));
-			// lösche alle inzidenten Kanten -> für alle Knoten, if containsedge mit u oder v then deleteedge || für directed
+			/*
+			 * delete all edges that are incident:
+			 * for all vertices
+			 * 		if there is an edge with the start- or the endpoint of the chosen edge
+			 * 			delete it
+			 */
 			for(int i = 0; i < copyVertices.size(); i++) {
 				currentVertex = copyVertices.get(i);
 				if (copyGraph.containsEdge(startVertexName, currentVertex) || copyGraph.containsEdge(currentVertex, startVertexName)) {
@@ -57,7 +96,8 @@ public class MaximalMatching implements Algorithm {
 	}
 	
 	@Override
-	public Graph getResult(){
+	public Graph getResult(Graph graph){
+		this.execute(graph);
 		return result;
 	}
 
