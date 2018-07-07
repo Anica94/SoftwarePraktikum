f53ce0b105c55491e6f09c775f12e7a62135e5b3
SPMT94 <39301328+SPMT94@users.noreply.github.com>
2018-07-07 16:39:44 +0200

    Update Build.java

--- Build.java
+++ Build.java
@@ -9,7 +9,8 @@ import java.util.ArrayList;
 public class Build {
 
 	// TODO #instanz von find conCom
-	ConnectedComponents conComps;
+	private ConnectedComponents conComps;
+	private Integer highestVertex;
 	/**
 	 * Saves the changes made by BUILD.
 	 */
@@ -20,6 +21,8 @@ public class Build {
 	 */
 	public Build() {
 		changes = new ArrayList<Operation>();
+		conComps = new ConnectedComponents();
+		highestVertex = new Integer(-1);
 	}
 
 
@@ -34,7 +37,7 @@ public class Build {
 	 * 
 	 * @throws NullPointerException if the tripleset or the leafset is <code> null</code>.
 	 */
-	public Pair<UndirectedGraph, Integer> build(ArrayList<Pair<Pair<Integer, Integer>, Integer>> triples, ArrayList<Integer> leaves) {
+	public Pair<UndirectedGraph, Integer> build(ArrayList<Pair<Pair<Integer, Integer>, Integer>> triples, ArrayList<Integer> leaves, Integer root) {
 		if (triples == null || leaves == null) { // auch wenn die leer? oder dann trotzdem laufenlassen und Ergebnis wird (hoffentlich) null?
     		throw new NullPointerException();
     	}
@@ -55,9 +58,22 @@ public class Build {
 		ArrayList<Pair<UndirectedGraph, Integer>> trees = new ArrayList();
 		UndirectedGraph tree = new UndirectedGraph(); // result
 		UndirectedGraph currentTree;
-		Integer root = new Integer(-1); // result
+		//Integer root = root; // result
 		Integer currentRoot;
-		
+		for (int i = 0; i < leaves.size(); i++) {
+			currentRoot = leaves.get(i);
+			if (root <= currentRoot) {
+				root = currentRoot+1;
+			}
+		}
+		if(root<highestVertex) {
+			root=highestVertex+1;
+		}
+		else {//if(highestVertex<root) {
+			highestVertex=root;
+		}
+		//System.out.println(root);
+
 		/*
 		 * do BUILD
 		 * 	compute Aho graph
@@ -65,16 +81,24 @@ public class Build {
 		 */
 		ahoGraph = this.computeAhoGraph(triples, leaves);
 		// TODO 
-		System.out.println("hihi");
-		connectedComponents = this.computeConnectedComponents(ahoGraph); // Instanz
-		conComps = new ConnectedComponents();
-		changes.addAll(conComps.execute(ahoGraph));// statt null die changes von findConCom
-		// TODO END
+		//System.out.println("hihi");
+		changes.addAll(conComps.execute(ahoGraph));
+		connectedComponents = conComps.getConnectedComponents();
 		numberOfComponents = connectedComponents.size();
+		//System.out.println("num of com= "+numberOfComponents);
+		/*
+		for(int j=0;j<numberOfComponents;j++) {
+			newLeaves = connectedComponents.get(j);
+			//System.out.println("Component: "+j);
+			for(int k=0;k<newLeaves.size();k++) {
+				System.out.println("Blatt: "+newLeaves.get(k));
+			}
+		}*/
 		/*
 		 * the tripleset is consistent and the result is isomorphic to K_1
 		 */
 		if (numberOfComponents == 1 & leaves.size() == 1) {
+			//System.out.println("in if 1");
 			root = leaves.get(0);
 			tree.addVertex(root);
 			changes.add(new VertexOperation("build add", root));
@@ -89,13 +113,18 @@ public class Build {
 			return rootedTree;
 		}
 		else {
+			//System.out.println("recursive");
 			/*
 			 * recursive call of BUILD
 			 */
 			for (int i = 0; i < numberOfComponents; i++) {
 				newLeaves = connectedComponents.get(i);
+				//System.out.println("Component: "+i);
+				for(int j=0;j<newLeaves.size();j++) {
+					//System.out.println(newLeaves.get(j));
+				}
 				newTriples = this.computeNewTriples(triples, newLeaves);
-				trees.add(build(newTriples, newLeaves));
+				trees.add(build(newTriples, newLeaves, root));
 			}
 			/*
 			 * the tripleset is consitent
@@ -109,7 +138,11 @@ public class Build {
 					currentRoot = trees.get(i).getSecond();
 					if (root <= currentRoot) {
 						root = currentRoot+1;
+						
 					}
+				}
+				if(highestVertex<root) {
+					highestVertex=root;
 				}
 				/*
 				 *  attach all trees (results of the recursive call)
@@ -120,6 +153,8 @@ public class Build {
 					currentTree = trees.get(i).getFirst();
 					tree.getStartpoints().putAll(currentTree.getStartpoints());
 					currentRoot = trees.get(i).getSecond();
+					//System.out.println("cuurent root = " + currentRoot);
+					//System.out.println("root" + root);
 					tree.addEdge(root, currentRoot);
 					changes.add(new EdgeOperation("build add", root, currentRoot));
 				}
@@ -165,7 +200,8 @@ public class Build {
 		for (int i = 0; i < leaves.size(); i++) {
 			currentLeave = leaves.get(i);
 			ahoGraph.addVertex(currentLeave);
-			changes.add(new VertexOperation("choose", currentLeave));
+			//System.out.println(" aho add:" +currentLeave);
+			changes.add(new VertexOperation("aho add", currentLeave));
 		}
 		/*
 		 *  add edges between the 'leaves' if they belong to the 'leftside' of a given triple
@@ -179,22 +215,13 @@ public class Build {
 					y = currentTripleLeftside.getSecond();
 					if(leaves.contains(y)) { // y in leaves
 						ahoGraph.addEdge(x, y);
-						changes.add(new EdgeOperation("choose", x, y));
+						//System.out.println(" aho add edge:" + x + " " + y);
+						changes.add(new EdgeOperation("aho add", x, y));
 					}
 				}
 			//}
 		}
 		return ahoGraph;
-	}
-	
-	// schreibt Anica
-	/**
-	 * 
-	 */
-	private ArrayList<ArrayList<Integer>> computeConnectedComponents(UndirectedGraph ahoGraph) {// liste von liste von allen Knoten in einer Component?
-		conComps = new ConnectedComponents();
-		conComps.execute(ahoGraph);
-		return conComps.getConnectedComponents();
 	}
 	
 	/**
