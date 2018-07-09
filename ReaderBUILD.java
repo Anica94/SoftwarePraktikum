b5511b82c6775b57d4c82dee3d943985c548d95c
Anica94 <39301272+Anica94@users.noreply.github.com>
2018-07-09 20:39:34 +0200

    Update ReaderBUILD.java

--- ReaderBUILD.java
+++ ReaderBUILD.java
@@ -19,6 +19,8 @@ public class ReaderBUILD {
 	private static ArrayList<Pair<Pair<Integer, Integer>, Integer>> triples;
 	private static Pair<Integer, Integer> tripleLeftside;
 	private static Pair<Pair<Integer, Integer>, Integer> triple;
+	private static StringBuilder sbL = new StringBuilder();
+	private static StringBuilder sbR = new StringBuilder();
 	
 	public ReaderBUILD() {
 		leaves = null;
@@ -48,17 +50,25 @@ public class ReaderBUILD {
 		/*
 		 * fill leaves
 		 */
+		sbL.append("L = {");
 		for (int i = 0; i < numberOfLeaves; i++) {
 			currentLine = bReader.readLine();
 			if (currentLine.matches("\\d+")) {
 				leaves.add(Integer.parseInt(currentLine));
+				sbL.append(currentLine);
+				if(i < numberOfLeaves-1) {
+					sbL.append(", ");
+				}					
 			}else {
 				throw new IOException();
 			}
 		}
+		sbL.append("}");
+		System.out.println(sbL);
 		/*
 		 * fill triples
 		 */
+		sbR.append("R = {");
 		for (int i=0; i<numberOfTriples; i++) {
 			currentLine = bReader.readLine();
 			if (currentLine.matches("\\d+\\s\\d+\\s\\d+")){
@@ -66,11 +76,24 @@ public class ReaderBUILD {
 				tripleLeftside = new Pair<Integer, Integer>(Integer.valueOf(currentLineSplit[0]), Integer.valueOf(currentLineSplit[1]));
 				triple = new Pair<Pair<Integer,Integer>, Integer>(tripleLeftside, Integer.valueOf(currentLineSplit[2]));
 				triples.add(triple);
+				sbR.append("((");
+				sbR.append(currentLineSplit[0]);
+				sbR.append(",");
+				sbR.append(currentLineSplit[1]);
+				sbR.append("),");
+				sbR.append(currentLineSplit[2]);
+				sbR.append(")");
+				if(i < numberOfTriples-1) {
+					sbR.append(", ");
+				}
+				
 			}else {
 				throw new IOException();
 			}
 		}
-		//System.out.println("number of leaves= " + leaves.size());
+		sbR.append("}");
+		System.out.println(sbR);
+		System.out.println("number of leaves= " + leaves.size());
 		bReader.close();
 	}
 	
@@ -80,7 +103,7 @@ public class ReaderBUILD {
 	 * @return leafset
 	 */
 	public static ArrayList<Integer> getLeafset() {
-		//System.out.println("huhu");
+		System.out.println("huhu");
 		return leaves;
 	}
 	
@@ -91,5 +114,21 @@ public class ReaderBUILD {
 	 */
 	public static ArrayList<Pair<Pair<Integer, Integer>, Integer>> getTripleset() {
 		return triples;
+	}
+	
+	/**
+	 * 
+	 * @return leafset as String
+	 */
+	public static String getLeafsetPrint() {
+		return sbL.toString();
+	}
+	
+	/**
+	 * 
+	 * @return tripleset as String
+	 */
+	public static String getTriplesetPrint() {
+		return sbR.toString();
 	}
 }
