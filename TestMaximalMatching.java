478f25fe931b811d4bc96026a74f312072220b18
Anica94 <39301272+Anica94@users.noreply.github.com>
2018-07-12 00:42:41 +0200

    Update TestMaximalMatching.java

--- TestMaximalMatching.java
+++ TestMaximalMatching.java
@@ -1,6 +1,9 @@
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.TreeMap;
 
 import org.junit.Test;
 
@@ -13,6 +16,7 @@ import junit.framework.TestCase;
 public class TestMaximalMatching extends TestCase {
 	
 	private Graph graph;
+	private Graph result;
 	private MaximalMatching mm = new MaximalMatching();
 	private Reader reader = new Reader();
 	private ArrayList<Operation> changes = new ArrayList<>();
@@ -108,12 +112,51 @@ public class TestMaximalMatching extends TestCase {
 				assertFalse("mm.execute(graph) decides for the wrong type of an operation.", true);
 			}
 			*/
-		}	
-		assertTrue("mm.execute(graph) makes the expected changes.", true);
+		}
 	}
 	
+	/**
+	 * Tests if the result is right.
+	 */
 	public void testGetResult() {
+		/*
+		 * Test if the result is really a matching
+		 */
+		try {
+			graph = reader.read("C:\\Users\\Anica\\eclipse-workspace\\Graph_ST_AH\\src\\Textfiles\\Test\\MMStarCircle.txt");
+		} catch (IOException e) {
+			// is tested at TestReader
+		}
+		result = mm.getResult(graph);
+		ArrayList<Integer> vertices = graph.getVertices();
+		ArrayList<Integer> verticesResult = result.getVertices();
+		HashMap<Integer, TreeMap<Integer, Integer>> startpoints = graph.getStartpoints();
+		HashMap<Integer, TreeMap<Integer, Integer>> startpointsResult = result.getStartpoints();
+		Integer currentVerex;
+		Integer currentEndpoint;
+		boolean success;
 		
+		for(int i=0; i<verticesResult.size(); i++) {
+			currentVerex = verticesResult.get(i);
+			success = startpointsResult.get(currentVerex).size()<=1;
+			assertTrue("The result (mm.getResult(graph)) is no matching.", success);
+		}
+		
+		/*
+		 * Test if the resulting matching is really maximal
+		 */
+		for(int i=0; i<vertices.size(); i++) {
+			currentVerex = vertices.get(i);
+			TreeMap<Integer, Integer> startAdjacent =graph.getStartpoints().get(currentVerex);
+			Set<Integer> set = startAdjacent.keySet();
+			java.util.Iterator<Integer> itr = set.iterator();
+		    while (itr.hasNext()){
+		    	currentEndpoint = itr.next();
+		    	// if success is true, the current edge is not in M but should be
+		    	success = (startpointsResult.get(currentVerex).isEmpty() && startpointsResult.get(currentEndpoint).isEmpty());
+		    	assertFalse("The result (mm.getResult(graph)) is no maximal matching, there is an edge that should be in M as well.", success);
+		    }
+		}
 	}
 
 }
