52f7522a91e6628ba802d593ad430b5648ceb5de
Anica94 <39301272+Anica94@users.noreply.github.com>
2018-07-11 22:01:59 +0200

    Add files via upload

---
+++ TestMaximalMatching.java
@@ -0,0 +1,105 @@
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Anica
+ *
+ */
+public class TestMaximalMatching extends TestCase {
+	
+	private Graph graph;
+	private MaximalMatching mm = new MaximalMatching();
+	private Reader reader = new Reader();
+	private ArrayList<Operation> changes = new ArrayList<>();
+	private ArrayList<Operation> expectedChanges = new ArrayList<>();
+	private EdgeOperation currentChange, currentExpected;
+
+	@Test
+	public void testExecute() {
+		
+		/*
+		 * Test null
+		 */
+		
+		try {
+			mm.execute(graph);
+		} catch (NullPointerException e) {
+			assertTrue("NullPointerException was thrown.", true);
+		}
+		
+		/*
+		 * Test empty graph
+		 */
+		graph = new UndirectedGraph();
+		try {
+			mm.execute(graph);
+		} catch (IllegalArgumentException e) {
+			assertTrue("IllegalArgumentException.", true);
+		}
+		
+		/*
+		 * Test graph without edges
+		 */
+		try {
+			graph = reader.read("C:\\Users\\Anica\\eclipse-workspace\\Graph_ST_AH\\src\\Textfiles\\Test\\MMnoEdges.txt");
+		} catch (IOException e) {
+			// is tested at TestReader
+		}
+		changes = mm.execute(graph);
+		assertTrue("mm.execute(graph) did not make any changes.", changes.isEmpty());
+		
+		/*
+		 * Test graph with two components: a star and a circle
+		 */
+		try {
+			graph = reader.read("C:\\Users\\Anica\\eclipse-workspace\\Graph_ST_AH\\src\\Textfiles\\Test\\MMStarCircle.txt");
+		} catch (IOException e) {
+			// is tested at TestReader
+		}
+		changes = mm.execute(graph);
+		expectedChanges.add(new EdgeOperation("consider", 1, 2));
+		expectedChanges.add(new EdgeOperation("choose", 1, 2));
+		expectedChanges.add(new EdgeOperation("consider", 3, 4));
+		expectedChanges.add(new EdgeOperation("choose", 3, 4));
+		expectedChanges.add(new EdgeOperation("consider", 5, 6));
+		expectedChanges.add(new EdgeOperation("choose", 5, 6));
+		
+		assertFalse("mm.execute(graph) makes the wrong number of changes", changes.size()==expectedChanges.size());
+		for(int i=0; i<changes.size(); i++) {
+			currentChange = (EdgeOperation) changes.get(i);
+			currentExpected = (EdgeOperation) expectedChanges.get(i);
+			if(currentChange.getOperationType().equals(currentExpected.getOperationType())) {
+				if(currentChange.getOperationName().equals(currentExpected.getOperationName())) {
+					if(currentChange.getStartVertexName()==currentExpected.getStartVertexName()) {
+						if(currentChange.getEndVertexName()==currentExpected.getEndVertexName()) {
+							
+						}
+						else {
+							assertFalse("mm.execute(graph) decides for the wrong endpoint.", true);
+						}
+					}
+					else {
+						assertFalse("mm.execute(graph) decides for the wrong startpoint.", true);
+					}
+				}
+				else {
+					assertFalse("mm.execute(graph) decides for the wrong name of an operation.", true);
+				}
+			}
+			else {
+				assertFalse("mm.execute(graph) decides for the wrong type of an operation.", true);
+			}
+		}	
+		assertTrue("mm.execute(graph) makes the expected changes.", true);
+	}
+	
+	public void testGetResult() {
+		
+	}
+
+}
