c5b987a2614d299b1033fd78aef11bb03c619de1
SPMT94 <39301328+SPMT94@users.noreply.github.com>
2018-07-14 14:57:11 +0200

    Update Build.java

--- Build.java
+++ Build.java
@@ -1,15 +1,20 @@
 import java.util.ArrayList;
 
 /**
- * This algorithm is BUILD. It computes a rooted, phylog. tree distinctly leaf-labeled by a given leafset 
+ * This algorithm is BUILD. It computes a rooted, phylogenetic tree distinctly leaf-labeled by a given leafset 
  * consistent with all rooted triplets in a given tripleset, if one exists; otherwise null.
  * 
  * @author Sonja
  */
 public class Build {
 
-
+	/**
+	 * Algorithm used to compute connected components of the AhoGraph.
+	 */
 	private ConnectedComponents conComps;
+	/**
+	 * Container for the highest vertex name used.
+	 */
 	private Integer highestVertex;
 	/**
 	 * Saves the changes made by BUILD.
@@ -24,7 +29,6 @@ public class Build {
 		conComps = new ConnectedComponents();
 		highestVertex = new Integer(-1);
 	}
-
 
 	/**
 	 * BUILD for a given tripleset and a given leafset. The result is a pair of the resulting tree and its root, if such a tree exists;
@@ -42,22 +46,20 @@ public class Build {
     		throw new NullPointerException();
     	}
 		/*
-		 * variables for the Aho graph
+		 * variables used for the Aho graph
 		 */
 		UndirectedGraph ahoGraph;
 		ArrayList<ArrayList<Integer>> connectedComponents;
 		int numberOfComponents;
 		ArrayList<Integer> newLeaves;
 		ArrayList<Pair<Pair<Integer, Integer>, Integer>> newTriples;
-		
 		/*
 		 * variables for the (resulting) tree
 		 */
 		Pair<UndirectedGraph, Integer> rootedTree;
 		ArrayList<Pair<UndirectedGraph, Integer>> trees = new ArrayList();
-		UndirectedGraph tree = new UndirectedGraph(); // result
+		UndirectedGraph tree = new UndirectedGraph();
 		UndirectedGraph currentTree;
-		//Integer root = root; // result
 		Integer currentRoot;
 		for (int i = 0; i < leaves.size(); i++) {
 			currentRoot = leaves.get(i);
@@ -68,36 +70,22 @@ public class Build {
 		if(root<highestVertex) {
 			root=highestVertex+1;
 		}
-		else {//if(highestVertex<root) {
+		else {
 			highestVertex=root;
 		}
-		//System.out.println(root);
-
 		/*
 		 * do BUILD
 		 * 	compute Aho graph
 		 * 	compute connected components
 		 */
 		ahoGraph = this.computeAhoGraph(triples, leaves);
-		// TODO 
-		//System.out.println("hihi");
 		changes.addAll(conComps.execute(ahoGraph));
 		connectedComponents = conComps.getConnectedComponents();
 		numberOfComponents = connectedComponents.size();
-		//System.out.println("num of com= "+numberOfComponents);
-		/*
-		for(int j=0;j<numberOfComponents;j++) {
-			newLeaves = connectedComponents.get(j);
-			//System.out.println("Component: "+j);
-			for(int k=0;k<newLeaves.size();k++) {
-				System.out.println("Blatt: "+newLeaves.get(k));
-			}
-		}*/
 		/*
 		 * the tripleset is consistent and the result is isomorphic to K_1
 		 */
 		if (numberOfComponents == 1 & leaves.size() == 1) {
-			//System.out.println("in if 1");
 			root = leaves.get(0);
 			tree.addVertex(root);
 			changes.add(new VertexOperation("build add", root));
@@ -112,24 +100,18 @@ public class Build {
 			return rootedTree;
 		}
 		else {
-			//System.out.println("recursive");
 			/*
 			 * recursive call of BUILD
 			 */
 			for (int i = 0; i < numberOfComponents; i++) {
 				newLeaves = connectedComponents.get(i);
-				//System.out.println("Component: "+i);
-				for(int j=0;j<newLeaves.size();j++) {
-					//System.out.println(newLeaves.get(j));
-				}
 				newTriples = this.computeNewTriples(triples, newLeaves);
 				trees.add(build(newTriples, newLeaves, root));
 			}
 			/*
-			 * the tripleset is consitent
+			 * the tripleset is consistent
 			 */
 			if ( !trees.contains(null)) {
-				//////// unbedingt ÃƒÂ¼berprÃƒÂ¼fen!!!!!!!!!!!
 				/*
 				 * compute new root
 				 */
@@ -152,8 +134,6 @@ public class Build {
 					currentTree = trees.get(i).getFirst();
 					tree.getStartpoints().putAll(currentTree.getStartpoints());
 					currentRoot = trees.get(i).getSecond();
-					//System.out.println("cuurent root = " + currentRoot);
-					//System.out.println("root" + root);
 					tree.addEdge(root, currentRoot);
 					changes.add(new EdgeOperation("build add", root, currentRoot));
 				}
@@ -177,16 +157,15 @@ public class Build {
 	 * 
 	 * @return (undirected) AhoGraph.
 	 * 
-	 * @throws NullPointerException if the tripleset or the leafset is <code> null</code>.
+	 * @throws NullPointerException if the leafset is <code> null</code>.
 	 */
 	private UndirectedGraph computeAhoGraph(ArrayList<Pair<Pair<Integer, Integer>, Integer>> triples, ArrayList<Integer> leaves) {
-		//if (triples == null || leaves == null) { // auch wenn die leer? oder dann trotzdem laufenlassen und Ergebnis wird (hoffentlich) null?
-    	//	throw new NullPointerException();
-    	//}
 		if(leaves == null) {
 			throw new NullPointerException();
 		}
-		
+		/*
+		 * variables used for the Aho graph
+		 */
 		UndirectedGraph ahoGraph = new UndirectedGraph();
 		Pair<Pair<Integer, Integer>, Integer> currentTriple;
 		Pair<Integer, Integer> currentTripleLeftside;
@@ -199,7 +178,6 @@ public class Build {
 		for (int i = 0; i < leaves.size(); i++) {
 			currentLeave = leaves.get(i);
 			ahoGraph.addVertex(currentLeave);
-			//System.out.println(" aho add:" +currentLeave);
 			changes.add(new VertexOperation("aho add", currentLeave));
 		}
 		/*
@@ -207,18 +185,15 @@ public class Build {
 		 */
 		for(int i = 0; i < triples.size(); i++) {
 			currentTriple = triples.get(i); 
-			//if(leaves.contains(currentTriple.getSecond())) { // z in leaves
 				currentTripleLeftside = currentTriple.getFirst();
 				x = currentTripleLeftside.getFirst();
 				if(leaves.contains(x)) { // x in leaves
 					y = currentTripleLeftside.getSecond();
 					if(leaves.contains(y)) { // y in leaves
 						ahoGraph.addEdge(x, y);
-						//System.out.println(" aho add edge:" + x + " " + y);
 						changes.add(new EdgeOperation("aho add", x, y));
 					}
 				}
-			//}
 		}
 		return ahoGraph;
 	}
@@ -231,10 +206,10 @@ public class Build {
 	 * 
 	 * @return new tripleset.
 	 * 
-	 * @throws NullPointerException if the tripleset or the leafset is <code> null</code>.
+	 * @throws NullPointerException if the leafset is <code> null</code>.
 	 */
 	private ArrayList<Pair<Pair<Integer, Integer>, Integer>> computeNewTriples(ArrayList<Pair<Pair<Integer, Integer>, Integer>> triples, ArrayList<Integer> leaves){
-		if (triples == null || leaves == null) { // auch wenn die leer? oder dann trotzdem laufenlassen und Ergebnis wird (hoffentlich) null?
+		if (leaves == null) {
     		throw new NullPointerException();
     	}
 		/*
