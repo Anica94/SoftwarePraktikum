d6de14e43c66e55ed45e1372e648c3e3696b1186
SPMT94 <39301328+SPMT94@users.noreply.github.com>
2018-07-14 10:51:20 +0200

    Update TestBuild.java

--- TestBuild.java
+++ TestBuild.java
@@ -42,14 +42,98 @@ public class TestBuild extends TestCase{
 	@Test
 	public void test() {
 		/*
-		 * test of empty or wrong input not necessary since ReaderBUILD will not produce wrong leafset and wrong tripleset
+		 * test of empty or wrong input not necessary since ReaderBUILD will not produce wrong or empty leafset and wrong tripleset
 		 */
+		/*
+		 * test empty tripleset
+		 */
+		try {
+			reader.read("C:\\Users\\Sonja\\eclipse-workspace\\SoftwarePraktikum\\src\\Textfiles\\Test\\BemptyTripleset.txt");
+		} catch (IOException e1) {
+			// is tested at TestReaderBUILD
+		}
+		leaves = reader.getLeafset();
+		triples = reader.getTripleset();
+		rootedTree = build.build(triples, leaves, new Integer(-1));
+        tree = rootedTree.getFirst();
+        root = rootedTree.getSecond();
+        /*
+         * 	produce expected changes
+         */
+        // aho graph
+     	expectedChanges.add(new VertexOperation("aho add", new Integer(1)));
+        expectedChanges.add(new VertexOperation("aho add", new Integer(2)));
+        expectedChanges.add(new VertexOperation("aho add", new Integer(3)));
+   		// connected components
+ 		expectedChanges.add(new VertexOperation("consider", new Integer(1)));
+ 		expectedChanges.add(new VertexOperation("choose", new Integer(1)));
+ 		expectedChanges.add(new VertexOperation("consider", new Integer(2)));
+ 		expectedChanges.add(new VertexOperation("choose", new Integer(2)));
+ 		expectedChanges.add(new VertexOperation("consider", new Integer(3)));
+ 		expectedChanges.add(new VertexOperation("choose", new Integer(3)));
+  			// recursive call 1
+     		// aho graph
+     		expectedChanges.add(new VertexOperation("aho add", new Integer(1)));
+   	        // connected components
+ 	        expectedChanges.add(new VertexOperation("consider", new Integer(1)));
+ 			expectedChanges.add(new VertexOperation("choose", new Integer(1)));
+ 			// build
+ 			expectedChanges.add(new VertexOperation("build add", new Integer(1)));
+			// recursive call 2
+     		// aho graph
+			expectedChanges.add(new VertexOperation("aho add", new Integer(2)));
+			// connected components
+			expectedChanges.add(new VertexOperation("consider", new Integer(2)));
+			expectedChanges.add(new VertexOperation("choose", new Integer(2)));
+			// build
+			expectedChanges.add(new VertexOperation("build add", new Integer(2)));
+			// recursive call 3
+ 			// aho graph
+ 			expectedChanges.add(new VertexOperation("aho add", new Integer(3)));
+ 			// connected components
+ 			expectedChanges.add(new VertexOperation("consider", new Integer(3)));
+ 			expectedChanges.add(new VertexOperation("choose", new Integer(3)));
+ 			//build
+ 			expectedChanges.add(new VertexOperation("build add", new Integer(3)));
+ 		// build
+		expectedChanges.add(new VertexOperation("build add", new Integer(4)));
+		expectedChanges.add(new EdgeOperation("build add", new Integer(4), new Integer(1)));
+		expectedChanges.add(new EdgeOperation("build add", new Integer(4), new Integer(2)));
+		expectedChanges.add(new EdgeOperation("build add", new Integer(4), new Integer(3)));
+		/*
+         * 	check changes
+         */
+        changes = build.getChanges();
+		success = changes.size()==expectedChanges.size();
+		assertTrue("build.build(triples, leaves, new Integer(-1)) makes the wrong number of changes", success);
+		for(int i = 0; i < changes.size(); i++) {
+			success = changes.get(i).getOperationType().equals(expectedChanges.get(i).getOperationType());
+			assertTrue("build.build(triples, leaves, new Integer(-1)) decides for the wrong type of an operation.", success);
+			if(changes.get(i).getOperationType().equals("vertex")) {
+				currentChangeV = (VertexOperation) changes.get(i);
+				currentExpectedV = (VertexOperation) expectedChanges.get(i);
+				success = currentChangeV.getOperationName().equals(currentExpectedV.getOperationName());
+				assertTrue("build.build(triples, leaves, new Integer(-1)) decides for the wrong name of an operation.", success);
+				success = currentChangeV.getVertexName().intValue() == currentExpectedV.getVertexName().intValue();
+				assertTrue("build.build(triples, leaves, new Integer(-1)) decides for the wrong vertex.", success);
+			}
+			else if(changes.get(i).getOperationType().equals("edge")) {
+				currentChangeE = (EdgeOperation) changes.get(i);
+				currentExpectedE = (EdgeOperation) expectedChanges.get(i);
+				success = currentChangeE.getOperationName().equals(currentExpectedE.getOperationName());
+				assertTrue("build.build(triples, leaves, new Integer(-1)) decides for the wrong name of an operation.", success);
+				success = currentChangeE.getStartVertexName().intValue() == currentExpectedE.getStartVertexName().intValue();
+				assertTrue("build.build(triples, leaves, new Integer(-1)) decides for the wrong startpoint.", success);
+				success = currentChangeE.getEndVertexName().intValue() == currentExpectedE.getEndVertexName().intValue();
+				assertTrue("build.build(triples, leaves, new Integer(-1)) decides for the wrong endpoint.", success);
+			}
+		}
 		/*
 		 * test inconsistent tripleset
 		 */
 		try {
 			reader.read("C:\\Users\\Sonja\\eclipse-workspace\\SoftwarePraktikum\\src\\Textfiles\\Test\\Binconsistent.txt");
-		} catch (IOException e) {
+		} catch (IOException e2) {
 			// is tested at TestReaderBUILD
 		}
 		leaves = reader.getLeafset();
@@ -109,7 +193,7 @@ public class TestBuild extends TestCase{
 		 */
 		try {
 			reader.read("C:\\Users\\Sonja\\eclipse-workspace\\SoftwarePraktikum\\src\\Textfiles\\Test\\BconsistentAllTriples.txt");
-		} catch (IOException e) {
+		} catch (IOException e3) {
 			// is tested at TestReaderBUILD
 		}
 		leaves = reader.getLeafset();
