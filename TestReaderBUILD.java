f96a8931195f6481f792fae4097ce15d390c7293
SPMT94 <39301328+SPMT94@users.noreply.github.com>
2018-07-14 14:56:14 +0200

    Update TestReaderBUILD.java

--- TestReaderBUILD.java
+++ TestReaderBUILD.java
@@ -5,21 +5,39 @@ import org.junit.Test;
 import junit.framework.TestCase;
 
 /**
- * 
+ * Test for the class ReaderBUILD.
  * 
  * @author Sonja
- *
  */
 public class TestReaderBUILD extends TestCase{
 
+	/**
+	 * Class to be tested.
+	 */
 	private ReaderBUILD reader = new ReaderBUILD();
-	private boolean success;
+	/**
+	 * Containers for the leaf- and tripleset to be tested.
+	 */
 	private ArrayList<Integer> leaves;
 	private String leavesPrint;
 	private ArrayList<Pair<Pair<Integer, Integer>, Integer>> triples;
 	private String triplesPrint;
+	/**
+	 * Container for testing.
+	 */
+	private boolean success;
 	
-
+	/**
+	 * Returns <code>true</code> a specified triple ((left, middle), right) is contained in a specified tripleset; <code>false</code> otherwise.
+	 * 
+	 * @param tripleset in which the specified triple is searched.
+	 * @param left part of the specified triple for which is searched.
+	 * @param middle part of the specified triple for which is searched.
+	 * @param right part of the specified triple for which is searched.
+	 * 
+	 * @return <code>true</code> if the specified tripleset contains the specified triple;
+	 * <code>false</code> otherwise.
+	 */
 	private boolean containsTriple(ArrayList<Pair<Pair<Integer, Integer>, Integer>> tripleset, Integer left, Integer middle, Integer right) {
 		Pair<Integer,Integer> currentLeftside;
 		for(Pair<Pair<Integer, Integer>, Integer> t : tripleset) {
@@ -37,7 +55,6 @@ public class TestReaderBUILD extends TestCase{
 	
 	@Test
 	public void testEmptyConstructor() {
-
 		assertNull("Leafset should be null after call to constructor.", reader.getLeafset());
 		assertNull("Tripleset should be null after call to constructor.", reader.getTripleset());
 	}
@@ -49,42 +66,52 @@ public class TestReaderBUILD extends TestCase{
 		 */	
 		try {
 			reader.read("C:\\Users\\Sonja\\eclipse-workspace\\SoftwarePraktikum\\src\\Textfiles\\Test\\RemptyFile.txt");
+			fail("expected IOException");
 		} catch (IOException e) {
-			assertTrue("IOException was thrown.", true);
+			// ignore, this exception is expected
 		}
 		/*
 		 * test wrong format
+		 * 	RBwrongFormat.txt: wrong number of leaves
+		 * 	RBwrong.txt: triple with not existing leaf
+		 * 	RBwrong2.txt: random text
+		 * 	RBwrong3.txt: graph text file (edge to not existing endpoint)
 		 */
 		try {
 			reader.read("C:\\Users\\Sonja\\eclipse-workspace\\SoftwarePraktikum\\src\\Textfiles\\Test\\RBwrongFormat.txt");
+			fail("expected IOException");
 		} catch (IOException e) {
-			assertTrue("IOException was thrown.", true);
+			// ignore, this exception is expected
 		}
 		try {
 			reader.read("C:\\Users\\Sonja\\eclipse-workspace\\SoftwarePraktikum\\src\\Textfiles\\Test\\RBwrong.txt");
+			fail("expected IllegalArgumentException");
 		} catch (IllegalArgumentException e) {
-			assertTrue("IllegalArgumentException was thrown.", true);
+			// ignore, this exception is expected
 		} catch (IOException e) {
-			assertFalse("IOException was thrown but not expected.", false);
+			assertFalse("IOException was thrown but not expected.", true);
 		}
 		try {
 			reader.read("C:\\Users\\Sonja\\eclipse-workspace\\SoftwarePraktikum\\src\\Textfiles\\Test\\RBwrong2.txt");
+			fail("expected IOException");
 		} catch (IOException e) {
-			assertTrue("IOException was thrown.", true);
+			// ignore, this exception is expected
 		}try {
 			reader.read("C:\\Users\\Sonja\\eclipse-workspace\\SoftwarePraktikum\\src\\Textfiles\\Test\\RBwrong3.txt");
+			fail("expected IOException");
 		} catch (IOException e) {
-			assertTrue("IOException was thrown.", true);
+			// ignore, this exception is expected
 		}
 		/*
 		 * test empty leafset
 		 */
 		try {
 			reader.read("C:\\Users\\Sonja\\eclipse-workspace\\SoftwarePraktikum\\src\\Textfiles\\Test\\RBemptyLeafset.txt");
-		} catch (IllegalArgumentException e1) {
-			assertTrue("IllegalArgumentException was thrown.", true);
+			fail("expected IllegalArgumentException");
+		} catch (IllegalArgumentException e) {
+			// ignore, this exception is expected
 		} catch (IOException e) {
-			assertFalse("IOException was thrown but not expected.", false);
+			assertFalse("IOException was thrown but not expected.", true);
 		}
 		/*
 		 * test empty tripleset
@@ -92,7 +119,7 @@ public class TestReaderBUILD extends TestCase{
 		try {
 			reader.read("C:\\Users\\Sonja\\eclipse-workspace\\SoftwarePraktikum\\src\\Textfiles\\Test\\RBemptyTripleset.txt");
 		} catch (IOException e) {
-			assertFalse("IOException was thrown but not expected.", false);
+			assertFalse("IOException was thrown but not expected.", true);
 		}
 		triples= reader.getTripleset();
 		success = triples.size() == 0;
@@ -103,7 +130,7 @@ public class TestReaderBUILD extends TestCase{
 		try {
 			reader.read("C:\\Users\\Sonja\\eclipse-workspace\\SoftwarePraktikum\\src\\Textfiles\\Test\\RBright.txt");
 		} catch (IOException e) {
-			assertFalse("IOException was thrown but not expected.", false);
+			assertFalse("IOException was thrown but not expected.", true);
 		}
 		/*
 		 *  check leaves
@@ -136,6 +163,5 @@ public class TestReaderBUILD extends TestCase{
 		success = triplesPrint.equals("R = {((1,2),5), ((1,2),4)}");
 		assertTrue("reader.read(textfile) prints a wrong tripleset.", success);
 	}
-
 }
 
