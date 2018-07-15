fb631d211d002e8e31e2b1b883bbb3b1cac88a11
Anica94 <39301272+Anica94@users.noreply.github.com>
2018-07-15 14:20:26 +0200

    Update Status.java

--- Status.java
+++ Status.java
@@ -1,9 +1,16 @@
 import java.util.Vector;
 
 /**
+ * This is a class that contains a list of possible statuses of the software:
+ * "Status", meaning that no other specific status is chosen, "runs Breadth-First Search", 
+ * "runs Depth-First Search", "runs Find Connected Components", "runs Topological Sort", 
+ * "runs Minimum Spanning Tree", "runs Maximal Matching" and "runs BUILD" for the 
+ * respective algorithms, "add vertex", "add edge", "delete vertex", "delete edge" and 
+ * "move" for editing the graph with the respective operation, "open file" and "save file" 
+ * for opening or saving a file, "Wrong file. See documentation for more information." and 
+ * "Wrong input. See documentation for more information." for warnings.
  * 
  * @author Anica
- *
  */
 
 public class Status {
@@ -46,16 +53,25 @@ public class Status {
 		//15
 		status.add("runs Find Connected Components");
 		//16
-		status.add("Wrong file. Press 'help' for more information.");
+		status.add("Wrong file. See documentation for more information.");
 		//17
-		status.add("Wrong input. Press 'help' for more information.");
+		status.add("Wrong input. See documentation for more information.");
 	}
 	
+	/**
+	 * Getter fot the current status.
+	 * @param number number of the current status.
+	 * @return current status.
+	 */
 	public String getStatus(int number) {
 		this.number = number;
 		return status.elementAt(number);
 	}
 	
+	/**
+	 * Getter for the current status number.
+	 * @return current status number.
+	 */
 	public int getStatusNumber() {
 		return number;
 	}
