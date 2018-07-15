1addb93133c72b0083c184840af375ae3715a111
Anica94 <39301272+Anica94@users.noreply.github.com>
2018-07-15 14:14:37 +0200

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -2,15 +2,27 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 /**
+ * This is a class that creates and draws circles (for the vertices of a graph).
+ * A circle contains a name, an x- and y-coordinate and a radius.
  * 
  * @author Anica
- *
  */
 
 public class Circle {
+	/**
+	 * Containers for the name, x- and y-coordinate and radius of the circle.
+	 */
 	private Integer name;
 	private int x, y, r;
 	
+	/**
+	 * Constructor of the class which creates a new circle with name <tt>name<tt>, radius <tt>r<tt>,
+	 * x-coordinate <tt>x<tt> and y-coordinate <tt>y<tt>.
+	 * @param name
+	 * @param r
+	 * @param x
+	 * @param y
+	 */
 	public Circle(Integer name, int r, int x, int y) {
 		this.name = name;
 		this.x = x;
@@ -18,6 +30,11 @@ public class Circle {
 		this.r = r;
 	}
 	
+	/**
+	 * Draws a circle in the specified color.
+	 * @param g graphical object.
+	 * @param c color in which the circle should be drawn.
+	 */
 	public void draw(Graphics g, Color c) {
 		g.setColor(c);
 		g.fillOval(x-r, y-r, r*2, r*2);
