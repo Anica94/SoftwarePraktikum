673803fd3f170423821ea1da172f9d050bb88978
Anica94 <39301272+Anica94@users.noreply.github.com>
2018-07-15 14:15:10 +0200

    Update ConnectedComponents.java

--- ConnectedComponents.java
+++ ConnectedComponents.java
@@ -1,30 +1,38 @@
 import java.util.ArrayList;
 
 /**
+ * This algorithm finds the connected components of a given graph.
+ * 
  * @author Anica
- *
  */
 public class ConnectedComponents implements Algorithm {
 	/**
 	 * Saves the result of the algorithm as a graph.
 	 */
 	private Graph result;
+	/**
+	 * Container for the result of the DFS.
+	 */
 	private Graph resultDFS;
 	
+	/**
+	 * Container for the algorithm DFS.
+	 */
 	private DFS dfs = new DFS();
+	/**
+	 * Container for the connected components.
+	 */
 	private ArrayList<ArrayList<Integer>> connectedComponents;
 
+	/**
+	 * Produces an algorithm for ConnectedComponents with an empty result.
+	 */
 	public ConnectedComponents() {
-		
-		// TODO Auto-generated constructor stub
+		result = null;
 	}
 	
-	/* (non-Javadoc)
-	 * @see Algorithm#execute(Graph)
-	 */
 	@Override
 	public ArrayList<Operation> execute(Graph graph) {
-		connectedComponents = new ArrayList<>();
 		if (graph == null) {
     		throw new NullPointerException();
     	}
@@ -32,12 +40,22 @@ public class ConnectedComponents implements Algorithm {
 			throw new IllegalArgumentException();
 		}
 		
+		/*
+		 * produce arraylist of operations that will be done by the algorithm
+		 * produce containers of variables to use later
+		 */	
 		ArrayList<Operation> changes = new ArrayList<Operation>();
+		connectedComponents = new ArrayList<>();
 		ArrayList<Operation> currentChanges = new ArrayList<Operation>();
 		ArrayList<Integer> verticesConnectedComponent;
 		ArrayList<Integer> vertices = graph.getVertices();
 		Integer startvertex;
 		
+		/*
+		 * produce an empty graph that will contain the result
+		 * the result will be a graph containing all connected components 
+		 * (which is the graph itself)
+		 */
 		if(graph.typeOfGraph().equals("undirected")) {
 			result = new UndirectedGraph();
 		}
@@ -45,16 +63,29 @@ public class ConnectedComponents implements Algorithm {
 			result = new DirectedGraph();
 		}
 		
+		/*
+		 * find connected components:
+		 * 	while the list of vertices is not empty
+		 * 		run DFS for the first element of the list <tt>vertices<tt>
+		 * 		add the changes made by the DFS to this changes
+		 * 		add the result of DFS to the resulting graph
+		 * 		add the list of vertices of the connected component found by 
+		 * 			DFS as a new element to the list of connected components
+		 * 		remove all vertices of the connected component found by DFS
+		 * 			of the list of vertices
+		 * 
+		 * return the list of operations
+		 */
 		while(!vertices.isEmpty()) {
 			startvertex = vertices.get(0);
 			dfs = new DFS();
 			currentChanges = dfs.execute(graph, startvertex);
-			resultDFS = dfs.getResult(graph, startvertex);
 			changes.addAll(currentChanges);
+			resultDFS = dfs.getResult(graph, startvertex);
+			result.getStartpoints().putAll(resultDFS.getStartpoints());
 			verticesConnectedComponent = new ArrayList<>();
 			verticesConnectedComponent = dfs.getVerticesOfConnectedComponent();		
 			connectedComponents.add(verticesConnectedComponent);
-			result.getStartpoints().putAll(resultDFS.getStartpoints());
 			for(int i=0; i<verticesConnectedComponent.size(); i++) {
 				vertices.remove(verticesConnectedComponent.get(i));
 			}
@@ -62,15 +93,16 @@ public class ConnectedComponents implements Algorithm {
 		return changes;
 	}
 
-	/* (non-Javadoc)
-	 * @see Algorithm#getResult(Graph)
-	 */
 	@Override
 	public Graph getResult(Graph graph) {
 		this.execute(graph);
 		return result;
 	}
 	
+	/**
+	 * Getter for the list of connected components (with a list of vertices of each connected component).
+	 * @return list of connected components.
+	 */
 	public ArrayList<ArrayList<Integer>> getConnectedComponents(){
 		return connectedComponents;
 	}
