4dad63720da05239e002f1209e5316d8d39fab48
Anica94 <39301272+Anica94@users.noreply.github.com>
2018-07-15 14:18:44 +0200

    Update MaximalMatching.java

--- MaximalMatching.java
+++ MaximalMatching.java
@@ -5,9 +5,9 @@ import java.util.Map.Entry;
 import java.util.TreeMap;
 
 /**
- * This algorithm computes a maximal matching for an undirected Graph.
+ * This algorithm computes a maximal matching for an undirected graph.
  * 
- * @author Sonja
+ * @author Sonja, Anica
  */
 public class MaximalMatching implements Algorithm {
 	/**
@@ -33,49 +33,36 @@ public class MaximalMatching implements Algorithm {
 		}
 		/*
 		 * make copy of the graph to work on (contains E')
-		 * produce container of variables to use later
+		 * produce containers of variables to use later
 		 * produce arraylist of operations that will be done by the algorithm
-		 */
-		
+		 */		
 		ArrayList<Integer> vertices = graph.getVertices();
 		HashMap<Integer, TreeMap<Integer, Integer>> startpoints = graph.getStartpoints();
 		Integer currentVertex;
 		Graph copyGraph;
-		if(graph.typeOfGraph().equals("undirected")) {
-			copyGraph = new UndirectedGraph();
-			for(int i = 0; i < vertices.size(); i++) {
-				currentVertex = vertices.get(i);
-				copyGraph.addVertex(currentVertex);
-			}
-			for(int i = 0; i < vertices.size(); i++) {
-				currentVertex = vertices.get(i);
-				TreeMap<Integer, Integer> startAdjacent =graph.getStartpoints().get(currentVertex);
-				Set<Integer> set = startAdjacent.keySet();
-				java.util.Iterator<Integer> itr = set.iterator();
-			    while (itr.hasNext()){
-			    	copyGraph.addEdge(currentVertex, itr.next());
-			    }
-			}
+		copyGraph = new UndirectedGraph();
+		for(int i = 0; i < vertices.size(); i++) {
+			currentVertex = vertices.get(i);
+			copyGraph.addVertex(currentVertex);
 		}
-		else {
-			copyGraph = new DirectedGraph();
+		for(int i = 0; i < vertices.size(); i++) {
+			currentVertex = vertices.get(i);
+			TreeMap<Integer, Integer> startAdjacent =graph.getStartpoints().get(currentVertex);
+			Set<Integer> set = startAdjacent.keySet();
+			java.util.Iterator<Integer> itr = set.iterator();
+		    while (itr.hasNext()){
+		    	copyGraph.addEdge(currentVertex, itr.next());
+		    }
 		}
-		//Graph copyGraph = graph;
 		ArrayList<Integer> copyVertices = copyGraph.getVertices();
 		HashMap<Integer, TreeMap<Integer, Integer>> copyStartpoints = copyGraph.getStartpoints();
-		//Integer currentVertex;
 		Integer startVertexName = null;
 		Integer endVertexName = null;
 		ArrayList<Operation> changes = new ArrayList<Operation>();
 		/*
 		 * produce the empty matching (M) as graph without any edges
 		 */
-		if(copyGraph.typeOfGraph().equals("undirected")) {
-			result = new UndirectedGraph();
-		}
-		else {
-			result = new DirectedGraph();
-		}
+		result = new UndirectedGraph();
 		for(int i = 0; i < copyVertices.size(); i++) {
 			currentVertex = copyVertices.get(i);
 			result.addVertex(currentVertex);
@@ -93,12 +80,8 @@ public class MaximalMatching implements Algorithm {
 			 * delete this edge in the copyGraph (update E')
 			 * memorize this step as an operation 
 			 */
-			System.out.println("num vertices copy = "+ copyVertices.size());
 			for(int i = 0; i < copyVertices.size(); i++) {
 				currentVertex = copyVertices.get(i);
-				System.out.println("currentVertex = "+ currentVertex);
-				System.out.println("copyStartpoints.get(currentVertex).firstKey() = "+ copyStartpoints.get(currentVertex));
-
 				if (copyStartpoints.containsKey(currentVertex) && !copyStartpoints.get(currentVertex).isEmpty())
 				{
 					startVertexName = currentVertex;
@@ -122,9 +105,6 @@ public class MaximalMatching implements Algorithm {
 			 */
 			for(int i = 0; i < copyVertices.size(); i++) {
 				currentVertex = copyVertices.get(i);
-				//System.out.println("currentVertex = "+ currentVertex);
-				//System.out.println("startVertex = "+ startVertexName);
-				//System.out.println("endVertex = "+ endVertexName);
 				if (copyGraph.containsEdge(startVertexName, currentVertex) || copyGraph.containsEdge(currentVertex, startVertexName)) {
 					copyGraph.deleteEdge(startVertexName, currentVertex);
 				}
